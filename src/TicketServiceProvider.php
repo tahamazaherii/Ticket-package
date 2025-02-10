0563b17fa5f62e16493420c162daeda494c0587a
Taha <104025877+tahamazaherii@users.noreply.github.com>
2025-02-10 11:52:11 +0330

    Update TicketServiceProvider.php

--- src/TicketServiceProvider.php
+++ src/TicketServiceProvider.php
@@ -21,7 +21,7 @@ class TicketServiceProvider extends ServiceProvider
             __DIR__ . '/../routes' => base_path('routes'),
             __DIR__ . '/../src/Models' => app_path('Models'),
             __DIR__ . '/../src/Repositories' => app_path('Repositories'),
-            __DIR__ . '/../resources/views' => app_path('views'),
+            __DIR__ . '/../resources/views' => resource_path('views'),
             __DIR__ . '/../database/migrations' => base_path('database/migrations'),
             __DIR__ . '/../database/seeders' => base_path('database/seeders'),
             __DIR__ . '/../public' => base_path('public'),
